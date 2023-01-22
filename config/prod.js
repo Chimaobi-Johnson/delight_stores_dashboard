2598a6b2f84499490811c23d1ac1631f0bb7959b
Chimaobi Wogu <woguchimaobi@gmail.com>
2023-01-22 21:59:32 +0000

    settings for heroku deployment

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  cloudinaryAPIKey: process.env.CLOUDINARY_API_KEY,
+  cloudinarySecretKey: process.env.CLOUDINARY_SECRET_KEY,
+  cloudinaryName: process.env.CLOUDINARY_NAME,
+};
--- index.js
+++ index.js
@@ -64,6 +64,16 @@ mongoose.connect(keys.mongoURI).then(connect => {
     console.log('Database Connected!')
   }).catch(err => console.log(err));
 
+if(process.env.NODE_ENV === 'production') {
+  // serve up production files
+  app.use(express.static('client/build'));
 
+  //serve up index.html file
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+  
 const PORT = process.env.PORT || 8000;
 app.listen(PORT);
