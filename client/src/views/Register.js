79922e141f64beecb6ac3b6321f5d4bdb8541c4d
Chimaobi Wogu <woguchimaobi@gmail.com>
2023-02-17 17:34:54 +0000

    added react hook form validation to register page

--- client/src/views/Register.js
+++ client/src/views/Register.js
@@ -1,7 +1,9 @@
 import React, { Component, useState } from "react";
 import { Form, InputGroup } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-import axios from 'axios';
+import { useForm, Controller } from "react-hook-form";
+
+import axios from "axios";
 // react-bootstrap components
 import {
   Badge,
@@ -14,68 +16,69 @@ import {
   Row,
   Col,
 } from "react-bootstrap";
+import { Input } from "reactstrap";
 
 function Register() {
 
-  const [inputData, setInputData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-    confirmPassword: ''
-  })
+
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
 
   const editTextHandler = (type, e) => {
-    setInputData(prevState => {
-        return {
-            ...prevState,
-            [type]: e.target.value
-        }
-    })
-  }
+    setInputData((prevState) => {
+      return {
+        ...prevState,
+        [type]: e.target.value,
+      };
+    });
+  };
 
-  const [errorMessages, setErrorMessages] = useState([])
+  const [errorMessages, setErrorMessages] = useState([]);
 
-  const [errorObj, setErrorObj] = useState({
-    
-  })
+  const [errorObj, setErrorObj] = useState({});
 
   const validateData = (inputObj) => {
-     for(const[key, value] of Object.entries(inputObj)) {
-        if(value === '') {
-            const errArr = [...errorMessages]
-            errArr.push('All fields are mandatory');
-            setErrorMessages(errArr)
-        }
-     }
-  }
-
-
-  const submitFormHandler = async () => {
-    if(inputData.password !== inputData.confirmPassword) {
-        setErrorMessage('Password and confirm password does not match!')
-        return
-    } else {
-        // validateData(inputData)
-        // if(errorMessages.length === 0) {
-        //     console.log('Form submitted')
-        // }
-        const formData = new FormData();
-        formData.append('firstName', inputData.firstName)
-        formData.append('lastName', inputData.lastName)
-        formData.append('email', inputData.email)
-        formData.append('password', inputData.password)
-
-        const result = await axios.post('/api/register', formData);
-        if(result.data.status === 200 || result.data.status === 201 ) {
-            alert('User created successfully')
-        } else {
-            alert('Error')
-            console.log(result)
-        }
+    for (const [key, value] of Object.entries(inputObj)) {
+      if (value === "") {
+        const errArr = [...errorMessages];
+        errArr.push("All fields are mandatory");
+        setErrorMessages(errArr);
+      }
     }
- 
-  }
+  };
+
+  const submitFormHandler = async (data) => {
+    console.log(data)
+    if (data.password !== data.confirmPassword) {
+      alert("Password and confirm password does not match!");
+      return;
+    } else {
+      // validateData(inputData)
+      // if(errorMessages.length === 0) {
+      //     console.log('Form submitted')
+      // }
+      const formData = new FormData();
+      formData.append("firstName", data.firstName);
+      formData.append("lastName", data.lastName);
+      formData.append("email", data.email);
+      formData.append("password", data.password);
+
+      const result = await axios.post("/api/register", formData);
+      if (result.data.status === 200 || result.data.status === 201) {
+        alert("User created successfully");
+      } else {
+        alert("Error");
+        console.log(result);
+      }
+    }
+  };
 
 
   return (
@@ -91,52 +94,68 @@ function Register() {
               <Form>
                 <Form.Group className="mb-3" controlId="formBasicFirstName">
                   <Form.Label>First Name</Form.Label>
-                  <Form.Control
-                    type="text"
-                    onChange={(e) => editTextHandler("firstName", e)}
-                    placeholder="Enter First Name"
+                  <Controller
+                    name="firstName"
+                    control={control}
+                     rules={{ required: true, minLength: 3 }}
+                    render={({ field }) => (
+                      <Form.Control {...field} type="text" placeholder="Enter First Name" />
+                    )}
                   />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicLastName">
                   <Form.Label>Last Name</Form.Label>
-                  <Form.Control
-                    type="text"
-                    onChange={(e) => editTextHandler("lastName", e)}
-                    placeholder="Enter Last Name"
+                  <Controller
+                    name="lastName"
+                    control={control}
+                     rules={{ required: true, minLength: 3 }}
+                    render={({ field }) => (
+                      <Form.Control {...field} type="text" placeholder="Enter Last Name" />
+                    )}
                   />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label>Email address</Form.Label>
-                  <Form.Control
-                    type="email"
-                    onChange={(e) => editTextHandler("email", e)}
-                    placeholder="Enter email"
+                  <Controller
+                    name="email"
+                    control={control}
+                     rules={{ required: true }}
+                    render={({ field }) => (
+                      <Form.Control {...field} type="email" placeholder="Enter Email" />
+                    )}
                   />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    onChange={(e) => editTextHandler("password", e)}
-                    placeholder="Password"
+                  <Controller
+                    name="password"
+                    control={control}
+                     rules={{ required: true, minLength: 8 }}
+                    render={({ field }) => (
+                      <Form.Control {...field} type="password" placeholder="Enter Password" />
+                    )}
                   />
                   {/* <Form.Text className="text-muted">
                     Password must not be less than 8 characters
                   </Form.Text> */}
                 </Form.Group>
-                <Form.Group className="mb-4" controlId="formBasicConfirmPassword">
+                <Form.Group
+                  className="mb-4"
+                  controlId="formBasicConfirmPassword"
+                >
                   <Form.Label>Confirm Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    onChange={(e) => editTextHandler("confirmPassword", e)}
-                    placeholder="Confirm Password"
+                  <Controller
+                    name="confirmPassword"
+                    control={control}
+                     rules={{ required: true, minLength: 8 }}
+                    render={({ field }) => (
+                      <Form.Control {...field} type="password" placeholder="Confirm Password" />
+                    )}
                   />
-                  <Form.Text className="text-muted">
-                    {}
-                  </Form.Text>
+                  <Form.Text className="text-muted">{}</Form.Text>
                 </Form.Group>
-                <Button variant="primary" onClick={() => submitFormHandler()}>
+                <Button variant="primary" onClick={handleSubmit(submitFormHandler)}>
                   Register
                 </Button>
                 <Form.Text className="text-muted">
