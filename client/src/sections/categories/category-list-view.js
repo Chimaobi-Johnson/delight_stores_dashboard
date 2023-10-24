7aa26dbe5cdf15fa98e1e354398acb5e6eb6a7d9
Chimaobi Wogu <woguchimaobi@gmail.com>
2023-10-24 04:46:23 +0100

    categories list page

--- client/src/sections/categories/category-list-view.js
+++ client/src/sections/categories/category-list-view.js
@@ -1,11 +1,97 @@
+import React, { useEffect, useState } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+
+import { useSettingsContext } from 'src/components/settings';
+
+import CustomBreadcrumbs from 'src/components/custom-breadcrumbs';
+import Iconify from 'src/components/iconify';
+
+// routes
+import { paths } from 'src/routes/paths';
+import { useRouter } from 'src/routes/hooks';
+import { RouterLink } from 'src/routes/components';
+
+import axios from 'axios';
 
 
 
 export default function CategoryListView() {
 
-    return (
-        <>
-            hello
-        </>
-    )
+  const settings = useSettingsContext();
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getCategories = () => {
+      axios
+        .get('/api/categories')
+        .then((data) => {
+          setCategories(data.data.categories);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    getCategories();
+  }, []);
+
+
+  return (
+    <Container maxWidth={settings.themeStretch ? false : 'lg'}>
+        <CustomBreadcrumbs
+          heading="List"
+          links={[
+            { name: 'Dashboard', href: paths.dashboard.categories.root },
+            { name: 'Category', href: paths.dashboard.categories.root },
+            { name: 'List' },
+          ]}
+          action={
+            <Button
+              component={RouterLink}
+              href={paths.dashboard.categories.new}
+              variant="contained"
+              startIcon={<Iconify icon="mingcute:add-line" />}
+            >
+              New Category
+            </Button>
+          }
+          sx={{
+            mb: { xs: 3, md: 5 },
+          }}
+        />
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Description</TableCell>
+            <TableCell align="right">Image</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {categories.length !== 0
+            ? categories.map((el) => (
+                <TableRow key={el.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row">
+                    {el.name}
+                  </TableCell>
+                  <TableCell align="right">{el.description}</TableCell>
+                  <TableCell align="right">{el.imageUrl}</TableCell>
+                </TableRow>
+              ))
+            : ''}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </Container>
+
+  );
 }
