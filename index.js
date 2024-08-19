3c1432b0e19ea7fb020a98a48f223cd0a4edc452
Chimaobi Wogu <woguchimaobi@gmail.com>
2024-08-19 05:05:29 +0100

    cors update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const cors = require('cors')
 
 const app = express();
 
-let whitelist = ['https://delighthomewarestores.com', 'http://localhost:3001']
+let whitelist = ['https://delighthomewarestores.com', 'https://delight-stores-backend-55d7b8765702.herokuapp.com', 'http://localhost:3001']
 let corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
